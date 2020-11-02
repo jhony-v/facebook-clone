bc2c8430e5ff2bb3b8894321786c609cc5026c18
Jhony Vega <jhonyvegacuya24@gmail.com>
2020-11-02 11:39:27 -0500

    Update component LinkButton

--- client/packages/fb-components/globals/Navigation/Atoms/LinkButton.tsx
+++ client/packages/fb-components/globals/Navigation/Atoms/LinkButton.tsx
@@ -1,20 +1,25 @@
+import { useRouter } from "next/dist/client/router";
 import React from "react";
 import { StyledItemNavigation, StyledLinkButton, StyledNotificationPoint } from "./elements";
+import Link, { LinkProps } from "next/link"
 
 type LinkButtonProps = {
   isSelected ?: boolean;
   notifications : number;
   icon: React.JSXElementConstructor<{size:number}>
-}
-const LinkButton = ({ isSelected, notifications, icon: IconComponent } : LinkButtonProps) => {
+} & LinkProps;
+const LinkButton = ({ isSelected, notifications, icon: IconComponent, ...restProps } : LinkButtonProps) => {
+  const { pathname } = useRouter();
   return (
     <StyledItemNavigation>
-      <StyledLinkButton isSelected={isSelected}>
-        {<IconComponent size={22} />}
-        {(notifications > 0) && (
+      <Link {...restProps}>
+        <StyledLinkButton isSelected={pathname === restProps.href}>
+          {<IconComponent size={22} />}
+          {(notifications > 0) && (
             <StyledNotificationPoint>{notifications}</StyledNotificationPoint>
-        )}
-      </StyledLinkButton>
+            )}
+        </StyledLinkButton>
+      </Link>
     </StyledItemNavigation>
   );
 };

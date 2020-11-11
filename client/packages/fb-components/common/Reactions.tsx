0b30eb1aa733b5a593cdf4ce892ebfb7e3a85f28
Jhony Vega <jhonyvegacuya24@gmail.com>
2020-11-11 12:56:24 -0500

    feat: Fix the path of assets

--- client/packages/fb-components/common/Reactions.tsx
+++ client/packages/fb-components/common/Reactions.tsx
@@ -6,12 +6,12 @@ const reaction = (image : string, alt: string)  => (props: AvatarTypes) => (
 );
 
 const Reactions = {
-  Like: reaction("assets/reactions/like.svg", "like"),
-  Love: reaction("assets/reactions/love.svg", "love"),
-  Sad: reaction("assets/reactions/sad.svg", "sad"),
-  Haha: reaction("assets/reactions/haha.svg", "haha"),
-  Care: reaction("assets/reactions/care.svg", "care"),
-  Angry: reaction("assets/reactions/angry.svg", "angry"),
+  Like: reaction("/assets/reactions/like.svg", "like"),
+  Love: reaction("/assets/reactions/love.svg", "love"),
+  Sad: reaction("/assets/reactions/sad.svg", "sad"),
+  Haha: reaction("/assets/reactions/haha.svg", "haha"),
+  Care: reaction("/assets/reactions/care.svg", "care"),
+  Angry: reaction("/assets/reactions/angry.svg", "angry"),
 };
 
 export default Reactions;

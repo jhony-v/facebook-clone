a43f031b4c367556c6ee2808d1c3b6b08e65bea5
Jhony Vega <jhonyvegacuya24@gmail.com>
2020-11-23 17:55:13 -0500

    feat: Create component UserResultSearchedCard

---
+++ client/packages/fb-components/globals/UserResultSearchedCard/index.tsx
@@ -0,0 +1,49 @@
+import Avatar from "@fb-components/common/Avatar";
+import Card from "@fb-components/common/Card";
+import FlexWrapper from "@fb-components/common/FlexWrapper";
+import TextLabel from "@fb-components/common/TextLabel";
+import Wrapper from "@fb-components/common/Wrapper";
+
+type UserResultSearchedCardProps = {
+  avatar: string;
+  fullName: string;
+  subtitle?: string;
+  description?: string;
+  children?: React.ReactNode;
+  rightComponent ?: React.ReactNode;
+};
+const UserResultSearchedCard = (props: UserResultSearchedCardProps) => {
+  return (
+    <Card variant="compact">
+      <Wrapper p="10px">
+        <FlexWrapper>
+          <Wrapper flexNone m="0 10px 0 0">
+            <Avatar src={props.avatar} dimension="60px" />
+          </Wrapper>
+          <FlexWrapper flexDirection="column" w="60%" justify="center">
+            <TextLabel weight textColor="vgTextBlack" textSize={300}>
+              {props.fullName}
+            </TextLabel>
+            <Wrapper>
+              {props.subtitle && (
+              <Wrapper m="5px 0 0">
+                  <TextLabel textSize={200}>{props.subtitle}</TextLabel>
+              </Wrapper>
+              )}  
+              {props.description && (
+                <Wrapper m="10px 0 0">
+                    <TextLabel textSize={300}>{props.description}</TextLabel>
+                </Wrapper>
+              )}
+            </Wrapper>
+            {props.children}
+          </FlexWrapper>
+          <Wrapper flexNone m="0 0 0 auto">
+              {props.rightComponent}
+          </Wrapper>
+        </FlexWrapper>
+      </Wrapper>
+    </Card>
+  );
+};
+export default UserResultSearchedCard;
--- client/pages/search/index.tsx
+++ client/pages/search/index.tsx
@@ -1,9 +1,11 @@
 import Wrapper from "@fb-components/common/Wrapper";
+import UserResultSearchedCard from "@fb-components/globals/UserResultSearchedCard";
 import LayoutDrawer from "@fb-components/layouts/LayoutsDrawer/LayoutDrawer";
 import LayoutDrawerGridColumn from "@fb-components/layouts/LayoutsDrawer/LayoutDrawerGridColumn";
 import LayoutDrawerLeftRail from "@fb-components/layouts/LayoutsDrawer/LayoutDrawerLeftRail";
 import ContainerMainNavigator from "@fb-containers/ContainerMainNavigator";
 import ContainerOptionsNavigation from "@fb-containers/ContainerOptionsNavigation";
+import faker from "faker";
 
 export default function Search() {
   return (
@@ -14,6 +16,16 @@ export default function Search() {
             <LayoutDrawerLeftRail compact>
               <ContainerOptionsNavigation />
             </LayoutDrawerLeftRail>
+            <div />
+            <div>
+            <UserResultSearchedCard
+              avatar={faker.random.image()}
+              fullName="Jhony Vega"
+              subtitle="16 friends"
+              description="Owner and Founder at HELLO Ma Khaing Ma Cho
+              University of Yangon Main"
+            />
+            </div>
           </LayoutDrawerGridColumn>
         </Wrapper>
       </LayoutDrawer>

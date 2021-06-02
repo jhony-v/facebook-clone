cf1843efd7770b74516443b07a599bb0eb5d4200
jhony-24 <jhonyvegacuya24@gmail.com>
2021-06-02 08:05:33 -0500

    feat(client-help): Add input to modal

--- client-help/src/components/HeaderNavigation/components/HelpCenterButtonSearchTopics/index.tsx
+++ client-help/src/components/HeaderNavigation/components/HelpCenterButtonSearchTopics/index.tsx
@@ -1,7 +1,22 @@
 import { useState } from "react";
 import { FiSearch } from "react-icons/fi";
+import { styled } from "../../../../lib/stitches.config";
 import BaseButtonAvatarAction from "../../../../ui/BaseButtonAvatarAction";
 import BaseModal from "../../../../ui/BaseModal";
+
+const InputEngineWrapper = styled("div", {
+  backgroundColor: "$button",
+  borderRadius: 10,
+  flex: 1,
+  marginRight: 20,
+  padding: 15,
+  display: "flex",
+  alignItems: "center",
+  "& input": {
+    marginLeft: 10,
+    width : "100%",
+  },
+});
 
 const HelpCenterButtonSearchTopics = () => {
   const [open, setOpen] = useState<boolean>(false);
@@ -12,14 +27,22 @@ const HelpCenterButtonSearchTopics = () => {
         icon={FiSearch}
         onClick={() => setOpen(true)}
       />
-      {open && <BaseModal>
-        <BaseModal.Header onClose={() => setOpen(false)} >
-            jhony
-        </BaseModal.Header>
-        <BaseModal.Body>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus, cum perspiciatis iusto iure ratione, quae nihil recusandae, voluptas maxime accusamus veniam cupiditate sit ipsum quidem! Dolorum ipsa non delectus quaerat?
-        </BaseModal.Body>
-      </BaseModal>}
+      {open && (
+        <BaseModal size="medium">
+          <BaseModal.Header onClose={() => setOpen(false)}>
+            <InputEngineWrapper>
+              <FiSearch />
+              <input placeholder="Search help articles..." />
+            </InputEngineWrapper>
+          </BaseModal.Header>
+          <BaseModal.Body>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus,
+            cum perspiciatis iusto iure ratione, quae nihil recusandae, voluptas
+            maxime accusamus veniam cupiditate sit ipsum quidem! Dolorum ipsa
+            non delectus quaerat?
+          </BaseModal.Body>
+        </BaseModal>
+      )}
     </div>
   );
 };

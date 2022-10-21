a9e9f82ef4d7eaca1a37e39884446f13f14198d5
Jhony Vega <jhonyvega.dev@gmail.com>
2022-10-21 10:48:40 -0500

    chore(client-app): update next config

--- apps/client-app/next.config.js
+++ apps/client-app/next.config.js
@@ -1,7 +1,11 @@
 const withBundleAnalizer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true"
+  enabled: process.env.ANALYZE === "true",
 });
 
-const config = withBundleAnalizer({});
+const config = withBundleAnalizer({
+  experimental: {
+    forceSwcTransforms: true
+  }
+});
 
 module.exports = config;
